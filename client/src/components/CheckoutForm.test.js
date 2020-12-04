df194ec0134602d8f574112e59d8be32ec87e8a5
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2020-12-04 13:51:38 -0500

    sprint 1 MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i)
+
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    const firstNameInput = screen.getByLabelText(/first name:/i)
+    const lastNameInput = screen.getByLabelText(/last name:/i)
+    const adressInput = screen.getByLabelText(/address:/i)
+    const cityInput = screen.getByLabelText(/city:/i)
+    const stateInput = screen.getByLabelText(/state:/i)
+    const zipInput = screen.getByLabelText(/last name:/i)
+    const button = screen.getByRole('button')
+
+    userEvent.type(firstNameInput, 'Benaiah')
+    userEvent.type(lastNameInput, 'Varner')
+    userEvent.type(adressInput, '29 saddle')
+    userEvent.type(cityInput, 'norwalk')
+    userEvent.type(stateInput, 'CT')
+    userEvent.type(zipInput, '06851')
+    userEvent.click(button)
+
+    const successMessage = screen.getByTestId('successMessage')
+    expect(successMessage).toBeInTheDocument()
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,28 @@
 // write your custom hook here to control your checkout form
+ import React, {useState} from 'react'
+ 
+ export const useForm = (Formvalues) => {
+     const [values, setValues] = useState(Formvalues);
+    
+
+     const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+
+     return [handleChanges, values]
+ }
+
+ 
+ export const useSubmit = () => {
+    const [showSuccessMessage, setSuccessMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSuccessMessage(true);
+      };
+
+
+     return [showSuccessMessage, handleSubmit]
+ }
+ 
+ 
